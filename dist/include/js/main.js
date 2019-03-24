6a4af576d14bb03661403371670d3e83e5a18f81
Toby Chow <tobychow98@gmail.com>
2019-03-23 21:42:29 -0500

    huge ui/ux update
    changed and added many features that improves UI/UX. Buttons will still need to be contained and responsive.

--- dist/include/js/main.js
+++ dist/include/js/main.js
@@ -1,8 +1,9 @@
+var color_inverted = true;
 $(document).ready(function(){
     $(".content").hover(function(){
         var styles={
-            filter: "blur(10px)",
-            '-webkit-filter': "blur(10px)",
+            filter: "blur(23px)",
+            '-webkit-filter': "blur(23px)",
             transition: "all 1000ms"
         }
         $(".showcase").css(styles);
@@ -13,8 +14,8 @@ $(document).ready(function(){
 $(document).ready(function(){
     $(".showcase").hover(function(){
         var styles={
-            filter: "blur(10px)",
-            '-webkit-filter': "blur(10px)",
+            filter: "blur(15px)",
+            '-webkit-filter': "blur(15px)",
             transition: "all 1000ms"
         }   
         $(".content").css(styles);
@@ -22,10 +23,40 @@ $(document).ready(function(){
     })
 })
 
+$(document).ready(function(){
+    $("body").click(function(){
+        if(!color_inverted){
+            color_inverted = true;
+            $("#logo").css(invert0());
+        }
+        else{
+            color_inverted = false;
+            $("#logo").css(invert100());
+        }
+    })
+})
 function unblurCSS(){
     var styles={
         filter: "blur(0px)",
         '-webkit-filter': "blur(0px)",
+        transition: "all 1000ms"
+    }
+    return styles;  
+}
+function invert100(){
+    var styles={
+        filter: "invert(100%)",
+        'webkit-filter': "invert(100%)",
+        transition: "all 500ms"
+    }
+    return styles;
+}
+
+function invert0(){
+    var styles={
+        filter: "invert(0%)",
+        'webkit-filter': "invert(0%)",
+        transition: "all 500ms"
     }
     return styles;
 }
