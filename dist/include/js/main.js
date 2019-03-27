921370af73114822995eb76a56b6da626adec072
Toby Chow <tobychow98@gmail.com>
2019-03-27 10:46:26 -0500

    Added modals
    Implemented many UI bugs and also added modal functionality to buttons

--- dist/include/js/main.js
+++ dist/include/js/main.js
@@ -1,4 +1,6 @@
 var color_inverted = true;
+
+// blurring background
 $(document).ready(function(){
     $(".content").hover(function(){
         var styles={
@@ -11,6 +13,7 @@ $(document).ready(function(){
     });
 })
 
+// bluring content
 $(document).ready(function(){
     $(".showcase").hover(function(){
         var styles={
@@ -23,6 +26,7 @@ $(document).ready(function(){
     })
 })
 
+// changing color of logo
 $(document).ready(function(){
     $("body").click(function(){
         if(!color_inverted){
@@ -35,6 +39,53 @@ $(document).ready(function(){
         }
     })
 })
+
+// about-me modal open
+$(document).ready(function(){
+    $("#about-me-button").click(function(){
+        $("#about-modal").css("display", "block");
+        $("#about-close").click(function(){
+            $("#about-modal").css("display", "none");
+        })
+    })
+})
+
+// close modal
+$(document).keydown(function(){
+    $(".modal").css("display", "none");
+})
+
+
+// projects modal open
+$(document).ready(function(){
+    $("#projects-button").click(function(){
+        $("#project-modal").css("display", "block");
+        $("#project-close").click(function(){
+            $("#project-modal").css("display", "none");
+        })
+    })
+})
+
+// contact modal open
+$(document).ready(function(){
+    $("#contact-button").click(function(){
+        $("#contact-modal").css("display", "block");
+        $("#contact-close").click(function(){
+            $("#contact-modal").css("display", "none");
+        })
+    })
+})
+
+// resume modal open
+$(document).ready(function(){
+    $("#resume-button").click(function(){
+        $("#resume-modal").css("display", "block");
+        $("#resume-close").click(function(){
+            $("#resume-modal").css("display", "none");
+        })
+    })
+})
+
 function unblurCSS(){
     var styles={
         filter: "blur(0px)",
@@ -47,7 +98,7 @@ function invert100(){
     var styles={
         filter: "invert(100%)",
         'webkit-filter': "invert(100%)",
-        transition: "all 500ms"
+        transition: "all 400ms"
     }
     return styles;
 }
@@ -56,7 +107,7 @@ function invert0(){
     var styles={
         filter: "invert(0%)",
         'webkit-filter': "invert(0%)",
-        transition: "all 500ms"
+        transition: "all 400ms"
     }
     return styles;
 }
