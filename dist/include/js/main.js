38b3884f3752a3d1f5d441f2eddc604c3e324548
Toby Chow <tobychow98@gmail.com>
2019-05-23 15:25:58 -0500

    Fixed Logo Animation

--- dist/include/js/main.js
+++ dist/include/js/main.js
@@ -1,69 +1,25 @@
-var color_inverted = true;
+var animated = false;
 
-// blurring background
+// logo animation
 $(document).ready(function(){
-    $(".content").hover(function(){
-        var styles={
-            filter: "blur(23px)",
-            '-webkit-filter': "blur(23px)",
-            transition: "all 1000ms"
-        }
-        $(".showcase").css(styles);
-        $(".content").css(unblurCSS());
-    });
-})
-
-// bluring content
-
-$(document).ready(function(){
-    $(".showcase").hover(function(){
-        var styles={
-            filter: "blur(8px)",
-            '-webkit-filter': "blur(8px)",
-            transition: "all 1000ms"
-        }   
-        $(".content").css(styles);
-        $(".showcase").css(unblurCSS());
-    })
-})
-
-// changing color of logo
-$(document).ready(function(){
-    $("body").click(function(){
-        if(!color_inverted){
-            color_inverted = true;
-            $("#logo").css(invert0());
+    $("#logo").click( function(){
+        var styles;
+        if(animated){
+            animated = false;
+            styles={    
+                '-webkit-animation': 'jello-horizontal-1 0.9s both',
+                    animation: "jello-horizontal-1 0.9s both"
+            }
         }
         else{
-            color_inverted = false;
-            $("#logo").css(invert100());
+            animated = true;
+            styles={    
+                '-webkit-animation': 'jello-horizontal 0.9s both',
+                    animation: "jello-horizontal 0.9s both"
+            }
         }
-    })
-})
+        $("#logo").css(styles);
+    });
+});
 
 
-function unblurCSS(){
-    var styles={
-        filter: "blur(0px)",
-        '-webkit-filter': "blur(0px)",
-        transition: "all 1000ms"
-    }
-    return styles;  
-}
-function invert100(){
-    var styles={
-        filter: "invert(100%)",
-        'webkit-filter': "invert(100%)",
-        transition: "all 400ms"
-    }
-    return styles;
-}
-
-function invert0(){
-    var styles={
-        filter: "invert(0%)",
-        'webkit-filter': "invert(0%)",
-        transition: "all 400ms"
-    }
-    return styles;
-}
